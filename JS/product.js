edbc6e34c550a9bd441809b1bb63ec6b5b00c604
Harsh Singh <harshnov80@gmail.com>
2023-06-18 22:26:44 +0530

    update content

--- JS/product.js
+++ JS/product.js
@@ -1,14 +1,30 @@
 let productList = document.querySelector("#product-list")
-
+const showEffect = (e) =>{
+    let parent = e.target
+    let normalImg = parent.querySelector('.normal')
+    let hoverImg = parent.querySelector('.onHover')
+    normalImg.style.opacity = "0";
+    hoverImg.style.opacity = "1";
+    hoverImg.style.height = "100%";
+}
+const removeEffect = (e) =>{
+    let parent = e.target
+    let normalImg = parent.querySelector('.normal')
+    let hoverImg = parent.querySelector('.onHover')
+    hoverImg.style.height = "0px";
+    hoverImg.style.opacity = "0";
+    normalImg.style.opacity = "1";
+}
 const loadData = async (path) => {
     try {
         const response = await fetch(path);
         const data = await response.json();
         data.forEach((element, index) => {
             productList.innerHTML += `
-            <div class="product" key=${index}>
+            <div class="product" key=${index} onmouseenter="showEffect(event)" onmouseleave="removeEffect(event)">
                 <div class="product-image">
-                    <img src=${element.image} alt="product">
+                    <img class="normal" src=${element.image} alt="product">
+                    <img class="onHover" src=${element.hovImage} alt="product" >
                 </div>
                 <div class="details">
                     <h4>${element.title}</h4>
